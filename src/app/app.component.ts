405ece52e10a9df18d1f69bc07c7f55626484b8a
Филипп <filipp.schastny@ozitag.com>
2020-12-10 12:59:45 +0300

    one way bind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'AngularSendBox';
+  title = 'Dinamic Title';
+  img = 'https://c7.hotpng.com/preview/933/638/374/react-components-javascript-redux-mobile-app-react-js.jpg';
+
+  constructor() {
+    setTimeout(() => {
+      console.log('Over');
+      this.img = 'https://im0-tub-by.yandex.net/i?id=ccad49d882765a82d302577e20e613c9&n=13';
+    }, 5000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {PostComponent} from './post/post.component';
+import { Post2Component } from './post2/post2.component';
+import { Post3Component } from './post3/post3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    Post2Component,
+    Post3Component
   ],
   imports: [
     BrowserModule
